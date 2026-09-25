b7e59eb401f0dd7cac04e70a3ca2c05c31ae825f
Venkateswarlu vankaraboina <230669206+vsiva8483-cpu@users.noreply.github.com>
2026-09-25 08:44:37 +0530

    Create app.py

---
+++ Backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "SnapStudy AI Backend is running!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
